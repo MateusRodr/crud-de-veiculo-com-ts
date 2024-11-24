9fca6e04c25f9dca7d50db43cf5b355f6547dead
MateusRodr <mr5508113@gmail.com>
2024-11-24 12:02:15 -0300

    Removendo arquivos rastreados que deveriam ser ignorados

--- src/config/database.ts
+++
@@ -1,9 +0,0 @@
-import {Sequelize} from 'sequelize';
-
-const sequelize = new Sequelize('vehicle', 'postgres', 'password',{
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: false,
-});
-
-export default sequelize
